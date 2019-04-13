7c57a01755fe2649dcf44930d2fbf6c9fc5e1395
root <root@DESKTOP-T11CEUN.localdomain>
2019-04-13 18:16:43 +0200

    Added models and packages

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const graphqlHttp = require('express-graphql')
-const { buildSchema } = require('graphql');
+const { buildSchema } = require('graphql')
+const mongoose = require('mongoose')
+
+const Event = require('./models/event')
 
 const app = express()
 
 app.use(bodyParser.json())
 
-app.use('/graphql', graphqlHttp({
-	schema: buildSchema(`
+app.use(
+	'/graphql',
+	graphqlHttp({
+		schema    : buildSchema(`
+		type Event {
+			_id: ID!
+			title: String!
+			description: String!
+			price: Float!
+			date: String!
+		} 
+
+		input EventInput {
+			title: String!
+			description: String!
+			price: Float!
+			date: String!
+		}
+
 		type RootQuery {
-			events: [String!]!
+			events: [Event!]!
 		}
 
 		type RootMutation {
-			createEvent(name: String): String
+			createEvent(eventInput: EventInput): Event
 		}
 
 		schema {
@@ -22,16 +42,50 @@ app.use('/graphql', graphqlHttp({
 			mutation: RootMutation 
 		}
 	`),
-	rootValue: {
-		events: () => {
-			return ['Romantic Cookin','Sailing','All-Night Coding']
+		rootValue : {
+			events      : () => {
+				return Event.find()
+					.then((events) => {
+						return events.map((event) => {
+							return { ...event._doc }
+						})
+					})
+					.catch((err) => {
+						throw err
+					})
+			},
+			createEvent : (args) => {
+				let { title, description, price, date } = args.eventInput
+				const event = new Event({
+					title       : title,
+					description : description,
+					price       : +price,
+					date        : new Date(date)
+				})
+				return event
+					.save()
+					.then((result) => {
+						console.log(result)
+						return { ...result._doc }
+					})
+					.catch((err) => {
+						console.log(err)
+						throw err
+					})
+			}
 		},
-		createEvent: (args) => {
-			const eventName = args.name;
-			return eventName;
-		}
-	},
-	graphiql: true
-}))
+		graphiql  : true
+	})
+)
+
+mongoose
+	.connect(
+		`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@gnomor-txiah.azure.mongodb.net/${process
+			.env.MONGO_DB}?retryWrites=true`
+	)
+	.then()
+	.catch((err) => {
+		console.log(err)
+	})
 
 app.listen(3000)
